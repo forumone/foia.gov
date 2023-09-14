fcdda32ede3dcaa1429b327eaf2579c75a0219c6
Steve Clay <steve@mrclay.org>
2023-09-14 17:34:05 -0400

    Full return to working build setup

--- js/settings/platform.js
+++ js/settings/platform.js
@@ -4,11 +4,11 @@ const api = {
   requestApiBaseURL: 'https://api.main-bvxea6i-oafzps2pqxjxw.us-2.platformsh.site/api',
   wizardApiURL: 'https://uat-api.foia.gov/doj-foia/models/ZBA2ow0/predict',
   // These are not secret, refer to https://github.com/18F/beta.foia.gov/tree/develop/docs/foia-api.md
-  apiProxyKey: '7aT5REJTpzjKoYKFuc9YIJLalHdYaif6ZqGkDPxG',
+  apiProxyKey: 'PvO2Wh2UppBQqGIxIvdKbogNRI0W0MVLFhkxjWfK',
   jsonApiKey: '7aT5REJTpzjKoYKFuc9YIJLalHdYaif6ZqGkDPxG',
 };
 
 export default {
   api,
-  appEnv: 'forum-one',
+  appEnv: 'platform',
 };
